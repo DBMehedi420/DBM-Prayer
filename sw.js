00eab15955684bb9cad22ce347bad08e013e705e
DBMehedi420 <mograh420mhasanmiya@gmail.com>
2026-03-09 00:36:16 +0600

    sw.js

--- sw.js
+++ sw.js
@@ -1,15 +1,38 @@
+const CACHE_NAME = 'islamic-pro-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/app.js',
+  '/manifest.json',
+  'https://unpkg.com/lucide@latest'
+];
+
+// ইনস্টল ইভেন্ট
 self.addEventListener('install', (e) => {
   e.waitUntil(
-    caches.open('static').then((cache) => {
-      return cache.addAll(['./', 'style.css', 'app.js']);
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
     })
   );
 });
 
+// এক্টিভ ইভেন্ট (পুরানো ক্যাশে ডিলিট করা)
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      );
+    })
+  );
+});
+
+// ফেচ ইভেন্ট (অফলাইন সাপোর্ট)
 self.addEventListener('fetch', (e) => {
   e.respondWith(
-    caches.match(e.request).then((response) => {
-      return response || fetch(e.request);
+    caches.match(e.request).then((res) => {
+      return res || fetch(e.request);
     })
   );
 });
